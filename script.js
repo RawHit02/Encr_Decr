899a12938ef425e1a46bd91612685642f78c882d
Rohit Panwar <107709247+RawHit02@users.noreply.github.com>
2023-09-04 17:47:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-function encrypt() {
-    var password = document.getElementById("secretKey").value;
-    if (password === "123456") {
-        var message = document.getElementById("inputText").value;
-        var encodedMessage = btoa(message);
-        document.getElementById("resultLabel").textContent = "⭐ Code Is ENCRYPTED ✔";
-        document.getElementById("outputText").value = encodedMessage;
-    } else if (password === "") {
-        alert("Error! Please Enter Secret Key");
-    } else {
-        alert("Invalid Secret Key");
-    }
-}
+       function caesarEncrypt(message, key) {
+            var encryptedMessage = "";
+            for (var i = 0; i < message.length; i++) {
+                var charCode = message.charCodeAt(i);
+                if (charCode >= 65 && charCode <= 90) { // Uppercase letters
+                    encryptedMessage += String.fromCharCode(((charCode - 65 + key) % 26) + 65);
+                } else if (charCode >= 97 && charCode <= 122) { // Lowercase letters
+                    encryptedMessage += String.fromCharCode(((charCode - 97 + key) % 26) + 97);
+                } else {
+                    encryptedMessage += message.charAt(i);
+                }
+            }
+            return encryptedMessage;
+        }
 
-function decrypt() {
-    var password = document.getElementById("secretKey").value;
-    if (password === "123456") {
-        var encodedMessage = document.getElementById("inputText").value;
-        var decodedMessage = atob(encodedMessage);
-        document.getElementById("resultLabel").textContent = "⭐ Code Is DECRYPTED ✔";
-        document.getElementById("outputText").value = decodedMessage;
-    } else if (password === "") {
-        alert("Error! Please Enter Secret Key");
-    } else {
-        alert("Invalid Secret Key");
-    }
-}
+        function caesarDecrypt(encryptedMessage, key) {
+            return caesarEncrypt(encryptedMessage, 26 - key); // Decryption is just encryption with the inverse key
+        }
 
-function reset() {
-    document.getElementById("inputText").value = "";
-    document.getElementById("secretKey").value = "";
-    document.getElementById("resultLabel").textContent = "";
-    document.getElementById("outputText").value = "";
-}
+        function encrypt() {
+            var password = document.getElementById("secretKey").value;
+            if (password === "123456") {
+                var message = document.getElementById("inputText").value;
+                var key = parseInt(document.getElementById("cipherKey").value);
+                var encodedMessage = caesarEncrypt(message, key);
+                document.getElementById("resultLabel").textContent = "⭐ Code Is ENCRYPTED ✔";
+                document.getElementById("outputText").value = encodedMessage;
+            } else if (password === "") {
+                alert("Error! Please Enter Secret Key");
+            } else {
+                alert("Invalid Secret Key");
+            }
+        }
+
+        function decrypt() {
+            var password = document.getElementById("secretKey").value;
+            if (password === "123456") {
+                var encodedMessage = document.getElementById("inputText").value;
+                var key = parseInt(document.getElementById("cipherKey").value);
+                var decodedMessage = caesarDecrypt(encodedMessage, key);
+                document.getElementById("resultLabel").textContent = "⭐ Code Is DECRYPTED ✔";
+                document.getElementById("outputText").value = decodedMessage;
+            } else if (password === "") {
+                alert("Error! Please Enter Secret Key");
+            } else {
+                alert("Invalid Secret Key");
+            }
+        }
+
+        function reset() {
+            document.getElementById("inputText").value = "";
+            document.getElementById("secretKey").value = "";
+            document.getElementById("cipherKey").value = "";
+            document.getElementById("resultLabel").textContent = "";
+            document.getElementById("outputText").value = "";
+        }
